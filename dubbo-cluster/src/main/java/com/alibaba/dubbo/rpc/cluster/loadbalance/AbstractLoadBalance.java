074a177ec2d4aa140fb10c188a0bb7d590176fb6
Pullock <pullocked@gmail.com>
2019-03-05 21:51:46 +0800

    LoadBalance

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/AbstractLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/AbstractLoadBalance.java
@@ -33,28 +33,46 @@ public abstract class AbstractLoadBalance implements LoadBalance {
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) {
         if (invokers == null || invokers.size() == 0)
             return null;
+        // 只有一个Invoker，可以直接返回，无需进行负载均衡
         if (invokers.size() == 1)
             return invokers.get(0);
+        // 负载均衡，子类实现
         return doSelect(invokers, url, invocation);
     }
 
     protected abstract <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation);
 
     protected int getWeight(Invoker<?> invoker, Invocation invocation) {
+        // 从url中获取权重，默认100
         int weight = invoker.getUrl().getMethodParameter(invocation.getMethodName(), Constants.WEIGHT_KEY, Constants.DEFAULT_WEIGHT);
         if (weight > 0) {
+            // 获取服务提供者启动时间戳
 	        long timestamp = invoker.getUrl().getParameter(Constants.TIMESTAMP_KEY, 0L);
 	    	if (timestamp > 0L) {
+	    	    // 计算服务提供者运行时长
 	    		int uptime = (int) (System.currentTimeMillis() - timestamp);
+	    		// 获取服务预热时间，默认10分钟
 	    		int warmup = invoker.getUrl().getParameter(Constants.WARMUP_KEY, Constants.DEFAULT_WARMUP);
+	    		// 如果服务运行时间小于预热时间，则重新计算服务权重，即降权
 	    		if (uptime > 0 && uptime < warmup) {
+	    		    // 重新计算服务器权重
 	    			weight = calculateWarmupWeight(uptime, warmup, weight);
 	    		}
 	    	}
         }
     	return weight;
     }
-    
+
+    /**
+     * 权重的计算过程，该过程主要用于保证当服务运行时长小于服务预热时间时，
+     * 对服务进行降权，避免让服务在启动之初就处于高负载状态。
+     * 服务预热是一个优化手段，与此类似的还有 JVM 预热。主
+     * 要目的是让服务启动后“低功率”运行一段时间，使其效率慢慢提升至最佳状态。
+     * @param uptime
+     * @param warmup
+     * @param weight
+     * @return
+     */
     static int calculateWarmupWeight(int uptime, int warmup, int weight) {
     	int ww = (int) ( (float) uptime / ( (float) warmup / (float) weight ) );
     	return ww < 1 ? 1 : (ww > weight ? weight : ww);
