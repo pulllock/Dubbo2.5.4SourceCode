aadd3067c2399249da673705a8f0ddad438a5c6d
Pullock <pullocked@gmail.com>
2019-03-05 19:00:39 +0800

    Cluster and Failover Cluster

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/FailoverCluster.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/FailoverCluster.java
@@ -31,7 +31,20 @@ public class FailoverCluster implements Cluster {
 
     public final static String NAME = "failover";
 
+    /**
+     * join其实是在服务消费者初始化阶段进行的
+     * 只是实例化一个FailoverClusterInvoker实例
+     * 实际使用的时候是在消费者进行远程调用的时候，
+     * 会首先调用Directory的list方法找到Invoker列表，
+     * 然后通过LoadBalance从Invoker列表中选择一个Invoker，
+     * 最后FailoverClusterInvoker将调用选中的Invoker进行实际调用。
+     * @param directory
+     * @param <T>
+     * @return
+     * @throws RpcException
+     */
     public <T> Invoker<T> join(Directory<T> directory) throws RpcException {
+        // 创建并返回FailoverClusterInvoker实例
         return new FailoverClusterInvoker<T>(directory);
     }
 
